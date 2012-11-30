0ac6675381427c6ec39d5998501501229e584374
Michael Hendricks <michael@ndrix.org>
2012-11-30 10:29:44 -0700

    Create pack file

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(miser).
+version('0.0.1').
+title('Build self-optimizing predicates').
+keywords([performance]).
+author( 'Michael Hendricks', 'michael@ndrix.org' ).
+packager( 'Michael Hendricks', 'michael@ndrix.org' ).
+maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
+home('https://github.com/mndrix/miser').
+download('https://github.com/mndrix/miser/downloads/miser-*.tgz').
